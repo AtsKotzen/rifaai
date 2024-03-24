bd1ecace84f6c5cb0c4d0f5a4f66b3d44b8d108f
atskotzen <atsaudio@gmail.com>
2024-03-24 00:49:24 -0300

    ajusta gasPrice

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import guit2 from './assets/guit2.jpeg';
 import guit3 from './assets/guit3.jpeg';
 import Navbar from './components/Navbar';
 
+
 function App() {
  const [manager, setManager] = useState("");
  const [players, setPlayers] = useState([]);
  const [balance, setBalance] = useState("");
- const valueInEther = 0.11; 
+ const valueInEther = .001; 
  const [message, setMessage] = useState("");
 
  useEffect(() => {
@@ -18,7 +19,7 @@ function App() {
       const manager = await lottery.methods.manager().call();
       const players = await lottery.methods.getPlayers().call();
       const balanceWei = await web3.eth.getBalance(lottery.options.address);
-      const balance = web3.utils.fromWei(balanceWei, 'ether');
+      const balance = web3.utils.fromWei(balanceWei, "ether");
        
       setManager(manager);
       setPlayers(players);
@@ -29,18 +30,17 @@ function App() {
  }, []);
 
  async function handleSubmit(event) {
-    event.preventDefault();
- 
+    event.preventDefault(); 
     const accounts = await web3.eth.getAccounts();
-    const valueInWei = web3.utils.toWei(valueInEther, 'ether');
-    
+    const valueInWei = web3.utils.toWei(valueInEther, "ether");
+    console.log(accounts[0]);
     setMessage("Waiting on transaction success...");
  
     try {
       await lottery.methods.enter().send({
         from: accounts[0],
         value: valueInWei,
-        gas: 30000000, // Configuração de gas
+        gas: 100000, // Configuração de gas
         gasPrice: web3.utils.toWei('1', 'gwei'), // Configuração de gasPrice
       });
  
@@ -59,8 +59,8 @@ function App() {
     try {
       await lottery.methods.pickWinner().send({
         from: accounts[0],
-        // gas: 1000000, // Configuração de gas
-        // gasPrice: web3.utils.toWei('1', 'gwei'), // Configuração de gasPrice
+        gas: 1000000, // Configuração de gas
+        gasPrice: 10000000, // Configuração de gasPrice
       });
  
       setMessage("A winner has been picked!");
@@ -70,56 +70,56 @@ function App() {
     }
  }
  return (
-    <div>
-      <Navbar />
-      <h2>Rifaai</h2>
-      <p>Rifa criada por Athus Oliveira</p>
-      <p>Carteira: {manager}</p>
-      <p>Atualmente existem {players.length} pessoas participando do Rifaai e ressignificando o que é ReFi</p>
-      <p>Total arrecadado em ETH: {balance}</p>
+  <div className="card">
+    <Navbar />
+    <h2 className="title">Rifaai</h2>
+    <p className="description">Rifa criada por Athus Oliveira</p>
+    <p className="description">Carteira: {manager}</p>
+    <p className="description">Atualmente existem {players.length} pessoas participando do Rifaai e ressignificando o que é ReFi</p>
+    <p className="description">Total arrecadado em ETH: {balance}</p>
 
-      <hr />
+    <hr />
 
-      <h4>Prêmio</h4>
-      <p>O prêmio para a rifa atual é uma guitarra elétrica usada. Aqui estão as características da guitarra:</p>
-      <ul>
-        <li>Muito confortável e macia</li>
-        <li>Marca: Gibson Epiphone</li>
-        <li>Modelo: Special</li>
-        <li>Pintura: Sunburst</li>
-      </ul>
-      <p>O preço de mercado da guitarra é R$1.000,00.</p>
-      <div>
-        <img src={guit1} width="200" alt='imagem do premio'/>
-        <img src={guit2} width="200" alt='imagem do premio'/>
-        <img src={guit3} width="200" alt='imagem do premio'/>
-      </div>
-
-      <hr />
-
-      <h4>Contribuição</h4>
-      <p>É importante mencionar que 10% da arrecadação será enviada automaticamente para a piscina comum de recursos compartilhados. Isso vai ajudar diversos projetos de impacto.</p>
-
-      <hr />
-
-      <form onSubmit={handleSubmit}>
-        <h4>Vai tentar a sorte?</h4>
-        <div>
-          <label>Valor do bilhete: 0.1 ETH + taxas de rede</label>
-        </div>
-        <button>Participar</button>
-      </form>
-
-      <hr />
-
-      <h4>Sortear vencedor</h4>
-      <button onClick={handleClick}>Iniciar sorteio!</button>
-
-      <hr />
-
-      <h1>{message}</h1>
+    <h4 className="subtitle">Prêmio</h4>
+    <p className="description">O prêmio para a rifa atual é uma guitarra elétrica usada. Aqui estão as características da guitarra:</p>
+    <ul className="list">
+      <li>Muito confortável e macia</li>
+      <li>Marca: Gibson Epiphone</li>
+      <li>Modelo: Special</li>
+      <li>Pintura: Sunburst</li>
+    </ul>
+    <p className="description">O preço de mercado da guitarra é R$1.000,00.</p>
+    <div className="image-container">
+      <img src={guit1} width="200" alt='imagem do premio'/>
+      <img src={guit2} width="200" alt='imagem do premio'/>
+      <img src={guit3} width="200" alt='imagem do premio'/>
     </div>
- );
+
+    <hr />
+
+    <h4 className="subtitle">Contribuição</h4>
+    <p className="description">É importante mencionar que 10% da arrecadação será enviada automaticamente para a piscina comum de recursos compartilhados. Isso vai ajudar diversos projetos de impacto.</p>
+
+    <hr />
+
+    <form onSubmit={handleSubmit}>
+      <h4 className="subtitle">Vai tentar a sorte?</h4>
+      <div>
+        <label className="description">Valor do bilhete: 0.1 ETH + taxas de rede</label>
+      </div>
+      <button className="button">Participar</button>
+    </form>
+
+    <hr />
+
+    <h4 className="subtitle">Sortear vencedor</h4>
+    <button className="button" onClick={handleClick}>Iniciar sorteio!</button>
+
+    <hr />
+
+    <h1 className="message">{message}</h1>
+  </div>
+);
 }
 
 export default App;
